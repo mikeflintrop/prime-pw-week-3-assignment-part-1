0ad26d361f238659a9091fc0e6d3ea7308184999
Mike Flintrop <mike.flintrop@gmail.com>
2022-05-16 13:57:08 -0500

    finished stretch goals first time

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -84,18 +84,23 @@ console.log('The foods are now', foodArray);
 // 4.e (STRETCH) TODO: Replace the second food in your array
 //      with another one of your favorite foods.
 //      Then log the updated array.
-
-
+foodArray.splice(1, 1, 'hamburgers');
+console.log('The foods are now', foodArray);
 
 // 4.f (STRETCH) TODO: Sort your favoriteFoods array
 //     in reverse alphabetical order. Log the array.
+foodArray.sort().reverse();
+console.log(foodArray);
 
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
+console.log(foodArray.join(' and '));
 
 // 4.h (STRETCH) TODO: Make a new array that combines 
 //     the favorite foods array with the animals array.
 //     Then log the new array.
 //     It should look something like:
 //     ['pizza', 'pasta', 'fish', 'cat', 'bird', 'dog']
+const comboArray = foodArray.concat(animalArray);
+console.log(comboArray);
